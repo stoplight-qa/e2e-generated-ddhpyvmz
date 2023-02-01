45c978b47da0481ec6fd47463ee4ce6f2c2e84aa
qa-ypgpf-0-vsru@example.com <qa-ypgpf-0-vsru@example.com>
2023-02-01 16:41:32 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-15055.stoplight-dev.com/api/v1/projects/cHJqOjIyNg/spectral.js?branch_slug=master&token=67a48d07-20ef-4db7-9c4c-fda44fae17a8";
+export default { extends: ruleset };
